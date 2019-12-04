816694848d0fc8bcfbeebe8b57bcc6e648431640
raphaelgyo <raphael.granizo@gmail.com>
2019-12-04 18:37:56 +0100

    correction

--- index.php
+++ index.php
@@ -1,24 +1,24 @@
-<!--ici on insère le PHP qui n'est pas lié à l'affichage-->
 <?php
-//Déclaration de la variable lastname
-$answer = 'yes';
-?>
+// Déclaration de la variable answer
+$answer='oui';
+ ?>
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
   <head>
-    <meta charset="utf-8" />
-    <title>PHP Partie 1 Exercice 5</title>
+    <meta charset="utf-8"/>
+    <title>PHP Partie 1 Exercice 5 Correction</title>
   </head>
   <body>
-    <p>
-    <!--ici on insère le PHP qui est lié à l'affichage-->
-      <?php
-      if ($answer == 'yes'){
-      echo 'Vous avez répondu'.' '.'oui';
-      }else{
-        echo 'Vous avez répondu'.' '.'non';
-      }
+    <?php
+    // Déclaration de l'instruction if. Si la réponse est oui alors on affiche le message suivant(vous avez...)
+    if ($answer=='Oui'){
       ?>
-    </p>
-  </body>
+      <p>Vous avez répondu oui</p>
+      <?php
+    // Sinon la réponse est non alors on affiche le message suivant (vous avez...)
+    }else{
+      ?>
+      <p>Vous avez répondu non</p>
+    <?php } ?>
+    </body>
 </html>
